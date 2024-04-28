cff4b6ec100cc00a2893ed63ef526d085459e8ad
phj1120 <hyeonj1998@naver.com>
2024-04-29 08:16:05 +0900

    컴포넌트 계층

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  typescript: {
+    shim: false,
+    typeCheck: true,
+  },
+  modules: ['nuxt-quasar-ui'],
+  quasar: {
+    /* */
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <div class="q-my-xl tex-center">
+      <div class="text-h4">Courses</div>
+      <p class="q-mt-sm text-grey-8">학습 해보세요!</p>
+    </div>
+    <div class="row q-col-gutter-lg">
+      <div v-for="n in 3" :key="n" class="col-12 col-md-4 col-sm-6">
+        <CourseCard />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>

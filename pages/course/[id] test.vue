cfabdbb990f1f71a54b16e5c483318143db7a273
phj1120 <hyeonj1998@naver.com>
2024-04-29 10:06:55 +0900

    vue with typescript

---
+++ pages/course/[id] test.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page padding>
+    <h2>Course</h2>
+    <AppCard>
+      <!-- <template v-slot:header> -->
+      <template #header>
+        <div>header</div>
+      </template>
+      <template #footer>
+        <div>footer</div>
+      </template>
+      <div>
+        <div>body</div>
+        <div class="text-sub">params: {{ $route.params }}</div>
+      </div>
+    </AppCard>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+console.log(route.params.id);
+</script>
+
+<style scoped></style>
